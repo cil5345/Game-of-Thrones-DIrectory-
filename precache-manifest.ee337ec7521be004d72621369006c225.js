0dd89dae074078d8219f7229ee34f9efe39a5834
Chris Lee <cil5345@gmail.com>
2020-04-07 10:04:01 -0400

    Updates

---
+++ precache-manifest.ee337ec7521be004d72621369006c225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637803596001ec20422cbe908e2109b9",
+    "url": "/Game-of-Thrones-Directory-/index.html"
+  },
+  {
+    "revision": "3c91e83a8a9f16082d04",
+    "url": "/Game-of-Thrones-Directory-/static/css/main.f462c51e.chunk.css"
+  },
+  {
+    "revision": "2d25038e018300ac9f76",
+    "url": "/Game-of-Thrones-Directory-/static/js/2.964fe810.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Game-of-Thrones-Directory-/static/js/2.964fe810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c91e83a8a9f16082d04",
+    "url": "/Game-of-Thrones-Directory-/static/js/main.fdbe4adc.chunk.js"
+  },
+  {
+    "revision": "9c2c2b3221b50a9a6587",
+    "url": "/Game-of-Thrones-Directory-/static/js/runtime-main.e5d0c432.js"
+  }
+]);
